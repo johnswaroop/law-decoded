21042bca09e9b546fea2a55888ac4d7461dc1117
Moses <moses7054@gmail.com>
2024-11-30 13:40:52 +0530

    removed grid

--- src/components/AboutSanjay.tsx
+++ src/components/AboutSanjay.tsx
@@ -4,24 +4,12 @@ const AboutSanjay = () => {
   return (
     <>
       {/* heingt = self */}
-      <div className="h-[754px] w-full flex flex-col items-center custom:h-[402px] custom:w-[1025px] custom:mx-auto">
+      <div className="h-[954px] w-full flex flex-col items-center custom:h-[402px] custom:w-[1025px] custom:mx-auto">
         <p className="text-[32px] font-georgia font-normal leading-[50px] mb-[35px] custom:text-[40px] custom:mb-[82px]">
           Our Social Media
         </p>
-        <div className="w-full h-[665.29px] overflow-hidden mx-auto custom:h-auto custom:hidden max-xs:w-[300px]">
-          <div className="grid grid-cols-3 h-full w-[601px] gap-[27.86px] mx-auto overflow-hidden items-center object-center custom:w-[1025px] custom:gap-x-[121px] custom:gap-y-[82px] max-xs:w-[300px] ">
-            <div className="w-[181.76px] h-[181.76px] bg-[#D9D9D9] custom:w-[261px] custom:h-[261px] "></div>
-            <div className="w-[181.76px] h-[181.76px] bg-[#D9D9D9] custom:w-[261px] custom:h-[261px] "></div>
-            <div className="w-[181.76px] h-[181.76px] bg-[#D9D9D9] custom:w-[261px] custom:h-[261px] "></div>
-            <div className="w-[181.76px] h-[181.76px] bg-[#D9D9D9] custom:w-[261px] custom:h-[261px]"></div>
-            <div className="w-[181.76px] h-[181.76px] bg-[#D9D9D9] custom:w-[261px] custom:h-[261px] "></div>
-            <div className="w-[181.76px] h-[181.76px] bg-[#D9D9D9] custom:w-[261px] custom:h-[261px]"></div>
-            <div className="w-[181.76px] h-[181.76px] bg-[#D9D9D9] custom:w-[261px] custom:h-[261px]"></div>
-            <div className="w-[181.76px] h-[181.76px] bg-[#D9D9D9] custom:w-[261px] custom:h-[261px]"></div>
-            <div className="w-[181.76px] h-[181.76px] bg-[#D9D9D9] custom:w-[261px] custom:h-[261px] "></div>
-          </div>
-        </div>
-        <div className="flex justify-between w-[1025px] max-custom:hidden">
+
+        <div className="flex justify-between w-[1025px] max-custom:flex-col max-custom:w-[300px] max-custom:items-center max-custom:h-[900px]">
           <div className="w-[261px] h-[261px] rounded-[6px] bg-[#D9D9D9]"></div>
           <div className="w-[261px] h-[261px] rounded-[6px] bg-[#D9D9D9]"></div>
           <div className="w-[261px] h-[261px] rounded-[6px] bg-[#D9D9D9]"></div>
