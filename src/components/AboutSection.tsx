769e876695ec2930ecd6b3954eb8a0b1a4f04a74
Moses <moses7054@gmail.com>
2024-12-02 23:55:34 +0530

    tab version

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -5,22 +5,21 @@ import LawyerImage from "@/assets/AboutSection/LawyerImage.png";
 const AboutSection = () => {
   return (
     <>
-      <div className="h-[907px] xs:h-[1000px] max-xs:h-[1075px] custom:h-[724px] flex flex-col items-center  custom:flex-row custom:items-center mb-[60px] custom:justify-center custom:gap-[139px]">
-        <div className="max-mid:hidden h-full w-1/2 flex justify-center items-center custom:w-[591px]">
-          <div className="w-[591px] h-[550px] relative overflow-hidden my-[80px] max-mid:hidden">
+      <div className="h-[907px] xs:h-[1000px] max-xs:h-[1075px] custom:h-[724px] md:h-[384px] flex flex-col items-center custom:flex-row md:flex-row custom:items-center md:items-center mb-[60px] custom:justify-center md:justify-center custom:gap-[139px] md:gap-[70px] md:mb-[40px]">
+        <div className="max-mid:hidden h-full w-1/2 flex justify-center items-center custom:w-[591px] md:w-[300px]">
+          <div className="w-[591px] h-[550px] relative overflow-hidden my-[80px] max-mid:hidden md:w-[300px] md:h-[250px]">
             <Image
               src={LawyerImage}
               alt="image"
-              fill
-              className="object-cover w-[591px] h-[550px]"
+              className="custom:object-cover md:object-fill  w-[591px] h-[550px] md:w-[300px] md:h-[250px]"
             />
           </div>
         </div>
-        <div className="flex flex-col items-center  custom:w-[462px] custom:justify-start  custom:h-[483px]">
-          <p className="font-georgia font-normal text-[32px] leading-[50px] text-center -tracking-[1px] custom:text-[40px] custom:mb-[24px] custom:w-[466px]  custom:text-start ">
+        <div className="flex flex-col items-center custom:w-[462px] md:w-[247.16px] custom:justify-start md:justify-start custom:h-[483px] md:h-[256.18px]">
+          <p className="font-georgia font-normal text-[32px] leading-[50px] text-center -tracking-[1px] custom:text-[40px] md:text-[21.22px] custom:mb-[24px] md:mb-[12px] custom:w-[466px] md:w-full custom:text-start md:text-start md:leading-[26.52px]">
             About Sajan Poovayya
           </p>
-          <div className="w-[336px] h-[313px] relative overflow-hidden my-[80px] mid:hidden max-xs:w-[300px] ">
+          <div className="w-[336px] h-[313px] relative overflow-hidden my-[80px] mid:hidden max-xs:w-[300px]">
             <Image
               src={LawyerImage}
               alt="image"
@@ -28,25 +27,28 @@ const AboutSection = () => {
               className="object-cover w-[336px] h-[313px] max-xs:w-[300px]"
             />
           </div>
-          <p className="font-inter font-normal text-[16px] leading-[28px] -tracking-[0.2px] px-[27px] custom:text-[18px] custom:w-[466px] custom:px-0">
+          <p className="font-inter font-normal text-[16px] leading-[28px] -tracking-[0.2px] px-[27px] custom:text-[18px] md:text-[9.55px] custom:leading-[14.85px] md:leading-[14.84px] custom:w-[466px] md:w-full custom:px-0 md:px-0">
             As a Senior Advocate, Sajan Poovayya is highly respected for his
             legal expertise and dedication. He is the leading voice in the
             country for tech laws, artificial intelligence, Web 3 and the whole
             new verse of laws emerging as technology transforms the world of
             law.
-            <br className="max-custom:hidden" />
-            <br className="max-custom:hidden" />
+            <br className=" max-md:hidden" />
+            <br className=" max-md:hidden" />
             He is also a strong advocate for the rights of junior lawyers,
             championing pay parity and equitable opportunities. Sajan believes
             that the future of the legal profession hinges on well-trained
             advocates and actively promotes mentorship, offering practical
             advice based on his extensive experience.
           </p>
-          <button className="mt-[28px] w-[129px] h-[48px] flex justify-center items-center bg-[#DD4826] custom:w-[466px] custom:h-[64px]">
-            <p className="text-white font-inter font-medium text-[16px] leading-[24px] -tracking-[0.16px] custom:hidden">
+          <button className="mt-[28px] w-[129px] h-[48px] flex justify-center items-center bg-[#DD4826] custom:w-[466px] md:w-[247.16px] custom:h-[64px] md:h-[33.95px] md:mt-[14px]">
+            <p className="text-white font-inter font-medium text-[16px] leading-[24px] -tracking-[0.16px] custom:hidden md:hidden">
               Read More
             </p>
-            <p className="text-white font-inter font-medium text-[16px] leading-[24px] -tracking-[0.16px] max-custom:hidden">
+            <p
+              className="text-white font-inter font-medium text-[16px] leading-[24px] -tracking-[0.16px] max-md:hidden md:text-[8.49px] md:leading-[12.73px]
+           "
+            >
               {" "}
               Book 30 minutes consultation
             </p>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,19 +6,19 @@ import Link from "next/link";
 import Image from "next/image";
 const NavBar = () => {
   return (
-    <div className="w-full h-[77px] bg-[#FFFBFA]">
-      <div className="w-full h-full px-[25px] flex justify-between items-center custom:px-[34px]">
+    <div className="w-full h-[77px] bg-[#FFFBFA] custom:h-[77px] md:h-[29.7px]">
+      <div className="w-full h-full px-[25px] flex justify-between items-center custom:px-[34px]  md:px-[17.6px]">
         {/* logo */}
-        <div className="w-[150px] h-[50.12px] overflow-hidden relative ">
+        <div className="w-[150px] h-[50.12px] overflow-hidden relative custom:w-[150px] custom:h-[50.12px] md:w-[58.3px] md:h-[19.5px]">
           <Image
             src={Logo}
             alt="image"
             fill
-            className="object-cover w-[150px] h-[50px]"
+            className="object-cover w-[150px] h-[50px] custom:w-[150px] custom:h-[50.12px]  md:w-[58.3px] md:h-[19.5px]"
           />
         </div>
         {/* middle buttons */}
-        <div className="font-outfit font-medium text-[15px] leading-[20px] max-md:hidden flex justify-between items-center gap-[16px] text-[#281D1B]">
+        <div className="font-outfit font-medium text-[15px] leading-[20px] max-md:hidden flex justify-between items-center gap-[16px] text-[#281D1B] max-custom:text-[7.96px] max-custom:leading-[10.61px] max-custom:gap-[8.49px]">
           <Link href={"/"}>Home</Link>
           <Link href={"/blogs"}>About Us</Link>
           <Link href={"/mentorship"}>Mentorship Programme</Link>
@@ -27,18 +27,18 @@ const NavBar = () => {
           <Link href={"/latestNews"}>Latest News</Link>
         </div>
         {/* Subscribe ContactUS */}
-        <div className="font-outfit font-medium text-[15px] leading-[20px] text-[#F72C00] flex justify-center gap-[32px] max-md:hidden">
+        <div className="font-outfit font-medium text-[15px] leading-[20px] text-[#F72C00] flex justify-center gap-[32px] max-md:hidden max-custom:text-[7.96px] max-custom:leading-[10.61px] ">
           <div className="flex justify-center items-center gap-[24px]">
             <Link href={"/"}>Subscribe</Link>
             <Link href={"/"}>Contact Us</Link>
           </div>
 
-          <div className="w-[24px] h-[24px] relative overflow-hidden">
+          <div className="w-[24px] h-[24px] relative overflow-hidden custom:w-[24px] custom:h-[24px] max-custom:w-[12.73px] max-custom:h-[12.73px]">
             <Image
               src={searchIcon}
               alt="image"
               fill
-              className="object-cover w-[24px] h-[24px]"
+              className="object-cover w-[24px] h-[24px] custom:w-[24px] custom:h-[24px] max-custom:w-[12.73px] max-custom:h-[12.73px]"
             />
           </div>
         </div>
