530ebead957132f9aad35ec1a3fec69e85e86557
“John <johnswaroop28@gmail.com>
2025-01-24 16:01:00 +0530

    res fix for ipad

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
       <AboutSection />
       <EfficientModel />
       <LawDecoded />
-      <AboutSanjay />
+      {/* <AboutSanjay /> */}
       <WhiteBox />
       <ILinkeBeing />
       <ReviewsAndLinks />
--- src/components/scaleProvider.tsx
+++ src/components/scaleProvider.tsx
@@ -5,10 +5,14 @@ function ScaleProvider({ children }: { children: ReactNode }) {
   useEffect(() => {
     if (window.innerWidth >= 2560) {
       document.body.style.zoom = "1.7"; // Adjust scaling factor as needed
+    } else if (window.innerWidth > 1023 && window.innerWidth < 1440) {
+      document.body.style.zoom = "1.4"; // Adjust scaling factor as needed
     } else if (window.innerWidth <= 1024 && window.innerWidth >= 1000) {
       document.body.style.zoom = "1.5"; // Adjust scaling factor as needed
-    } else if (window.innerWidth < 1000 && window.innerWidth >= 920) {
-      document.body.style.zoom = "1.3"; // Adjust scaling factor as needed
+    } else if (window.innerWidth < 1000 && window.innerWidth >= 900) {
+      document.body.style.zoom = "1.2"; // Adjust scaling factor as needed
+    } else if (window.innerWidth < 900) {
+      document.body.style.zoom = "1"; // Adjust scaling factor as needed
     }
   }, []);
 
